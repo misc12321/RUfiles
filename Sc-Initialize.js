8dbc7e5d5c6a8a7ca0e8919aef3573562992fc2f
chrislpatton <chrislpatton@gmail.com>
2017-03-23 11:02:10 -0400

    added js files

---
+++ Sc-Initialize.js
@@ -0,0 +1,9 @@
+// Add all onLoad and Init Functions In This File
+
+/*Start SC Click to Enlarge v3.0.0 init function calls*/
+var scMultiMain = false;
+scImgEnlargeInit();
+window.onresize = function(){
+    scWinResize();
+}
+/*End SC Click to Enlarge v3.0.0 init calls*/
